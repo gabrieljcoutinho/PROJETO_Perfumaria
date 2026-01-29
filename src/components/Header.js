8c18a3a440d6dbd92c8be56f7410e2c3581b12c2
Gabriel <gabrieljcoutinho@gmail.com>
2026-01-29 09:02:41 -0300

    logo da pagina

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../CSS/Header/header.css";
 
+import imgLogo from "../assets/imgs/logoPage.png"
+
 const Header = () => {
   return (
     <header className="header container">
       <ul className="ulHeader">
+      <img src={imgLogo} alt="" />
             <li><Link to="/">Home</Link></li>
             <li><Link to="/loja">Loja</Link></li>
             <li><Link to="/contato">Contato</Link></li>
