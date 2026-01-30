72ae6a600058d8891fcd496b77039bb0b45058e8
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-01-30 13:20:35 -0300

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../../src/CSS/Footer/index.css"
+
+const Footer = () => {
+  return (
+    <div className='Footer'>
+
+
+<h1>Footer</h1>
+
+
+        </div>
+  )
+}
+
+export default Footer
