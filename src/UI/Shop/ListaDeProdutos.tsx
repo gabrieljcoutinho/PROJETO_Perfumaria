5d874e8a3fe06567f973134acc32a232a6055289
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-02 11:32:48 -0300

    refazeno

--- src/UI/Shop/ListaDeProdutos.tsx
+++ src/UI/Shop/ListaDeProdutos.tsx
@@ -21,47 +21,7 @@ type Props = {}
 const ListaDeProdutos = (props: Props) => {
   return (
     <div className='listaDeProdutos'>
-
-
-            <div className="flexbox">
-
-    <div className="divsConteudo">
-    <img src={img1} alt="" />
-
-<img src={img2} alt="" />
-
-<img src={img3} alt="" />
-
-<img src={img4} alt="" />
-    </div>
-
-    <div className="divsConteudo">
-    <img src={img5} alt="" />
-
-<img src={img6} alt="" />
-
-<img src={img7} alt="" />
-
-<img src={img8} alt="" />
-        </div>
-
-        <div className="divsConteudo">
-        <img src={img9} alt="" />
-
-<img src={img10} alt="" />
-
-<img src={img11} alt="" />
-
-<img src={img12} alt="" />
-        </div>
-
-
-
-
-
-
-            </div>
-
+                    
     </div>
   )
 }
