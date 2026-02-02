1a8aa5ebd804339cb92cb5774c4c84fd5a7d8703
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-02 10:06:49 -0300

    sepradn ocotneudo

---
+++ src/UI/Home/BoasVindas.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+type Props = {}
+
+const BoasVindas = (props: Props) => {
+  return (
+    <div>
+
+<main className="boasVindasAoSite">
+            <h2 className='subtituliMainBoasVindas'>Welcome to Local Face</h2>
+
+          <p className="textoMainBoasvindas">
+            Welcome to Local Face Perfumes, where the spirit of victory and triumph come alive through scents that empower and inspire.  <br />
+            Our curated collection, aptly named "Victory Scented," is a celebration of success and elegance, designed to unleash your<br />
+            victorious essence. Indulge in the sweet taste of triumph with captivating fragrances that tell the tale of your achievements.<br />
+            At Local Face, we believe that every victory deserves a signature scent, and we are dedicated to providing unforgettable
+            fragrances that elevate your spirit and empower your journey.</p>
+
+        </main>
+    </div>
+  )
+}
+
+export default BoasVindas
