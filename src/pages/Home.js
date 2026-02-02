f63c169d339b971d8b2e8662d790d00c41be10ea
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-02 10:26:29 -0300

    separando conteudo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,8 +12,6 @@ import "../CSS/Home/mainPerfumesMaisRecentes.css"
 
 
 
-
-import img8 from "../assets/imgs/fundoPromocao.png"
 import img9 from "../assets/imgs/imgPerfumesRecentes.png"
 
 
@@ -22,6 +20,7 @@ import BoasVindas from "../UI/Home/BoasVindas"
 import NossosValores from "../UI/Home/NossosValores"
 import AmostraProdutos from "../UI/Home/AmostraProdutos"
 import MainColecoes from "../UI/Home/MainColecoes"
+import MainSecaoPromocao from "../UI/Home/MainSecaoPromocao"
 
 import Footer from "../components/Footer"
 
@@ -39,21 +38,9 @@ const Home = () => {
 
           <MainColecoes />
 
+          <MainSecaoPromocao />
 
 
-        <main className="mainSecaoPromocao">
-
-        <h2 className='tituloPromocao'>Perfume year-End Sale!</h2>
-
-        <h3 className="subTituloPromocao">Up to 50% OFF</h3>
-
-    <img src={img8} alt="" className='imgPromocao' />
-        <p className="paragrafoPerfumeEmPromocao">Discover and exquisite collections of premium perfumes at <br/>
-        umbelievable prices during our exclusive Perfume Sale!</p>
-        <main className="movimentacaoBtnComprarPerfumePromocao">
-      <button class="btn-brown">Know More</button>
-      </main>
-        </main>
 
 
 
