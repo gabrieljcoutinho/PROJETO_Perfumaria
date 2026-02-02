bbed097ca31ffac78b34651b5874e0e9b15dbfa4
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-02 10:06:43 -0300

    sepradn ocotneudo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -25,6 +25,7 @@ import img9 from "../assets/imgs/imgPerfumesRecentes.png"
 
 
 import ConteudoInicial from "../UI/Home/ConteudoInicial"
+import BoasVindas from "../UI/Home/BoasVindas"
 import Footer from "../components/Footer"
 
 const Home = () => {
@@ -33,17 +34,7 @@ const Home = () => {
 
           <ConteudoInicial />
 
-        <main className="boasVindasAoSite">
-            <h2 className='subtituliMainBoasVindas'>Welcome to Local Face</h2>
-
-          <p className="textoMainBoasvindas">
-            Welcome to Local Face Perfumes, where the spirit of victory and triumph come alive through scents that empower and inspire.  <br />
-            Our curated collection, aptly named "Victory Scented," is a celebration of success and elegance, designed to unleash your<br />
-            victorious essence. Indulge in the sweet taste of triumph with captivating fragrances that tell the tale of your achievements.<br />
-            At Local Face, we believe that every victory deserves a signature scent, and we are dedicated to providing unforgettable
-            fragrances that elevate your spirit and empower your journey.</p>
-
-        </main>
+          <BoasVindas />
 
           <main className="nossosValores">
             <img src={imgNossosValores} className='imgNossosValores' alt="" />
