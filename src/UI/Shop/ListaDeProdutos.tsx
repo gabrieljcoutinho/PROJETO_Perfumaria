28c0281a92d855581b7e239dd084f341fda88248
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-02 11:30:50 -0300

    conteudo

--- src/UI/Shop/ListaDeProdutos.tsx
+++ src/UI/Shop/ListaDeProdutos.tsx
@@ -26,27 +26,28 @@ const ListaDeProdutos = (props: Props) => {
             <div className="flexbox">
             <img src={img1} alt="" />
 
-<img src={img2} alt="" />
+            <img src={img2} alt="" />
 
-<img src={img3} alt="" />
+            <img src={img3} alt="" />
 
-<img src={img4} alt="" />
+            <img src={img4} alt="" />
 
-<img src={img5} alt="" />
+            <img src={img5} alt="" />
 
-<img src={img6} alt="" />
+            <img src={img6} alt="" />
 
-<img src={img7} alt="" />
+            <img src={img7} alt="" />
 
-<img src={img8} alt="" />
+            <img src={img8} alt="" />
 
-<img src={img9} alt="" />
+            <img src={img9} alt="" />
 
-<img src={img10} alt="" />
+            <img src={img10} alt="" />
 
-<img src={img11} alt="" />
+            <img src={img11} alt="" />
 
-<img src={img12} alt="" />
+            <img src={img12} alt="" />
+            
             </div>
 
     </div>
