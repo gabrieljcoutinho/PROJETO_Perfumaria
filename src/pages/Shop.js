d7f14c7be3640c13dba1e4912df5c258204ca395
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-02 10:51:58 -0300

    teste

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import MarcacaoDePagina from '../UI/Shop/MarcacaoDePagina.tsx'
 
 const Shop = () => {
   return (
     <div>
+      <MarcacaoDePagina />
 
-        
 
     </div>
   )
