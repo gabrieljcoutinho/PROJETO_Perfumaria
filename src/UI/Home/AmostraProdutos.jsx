4df0f349142335ac537db248790f926634f017d4
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-02 10:21:24 -0300

    seaprnco mains

---
+++ src/UI/Home/AmostraProdutos.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import imgProdutos from "../../../src/assets/imgs/Frame 280.png"
+
+type Props = {}
+
+const AmostraProdutos = (props: Props) => {
+  return (
+    <div>     <main className="mainAmostraProdutos">
+    <h1 className="tituloProdutos">Beste selling products</h1>
+<img src={imgProdutos}  className='imgProdutos' alt="" />
+  </main></div>
+  )
+}
+
+export default AmostraProdutos
