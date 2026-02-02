4632c30b7e261aab6208427506147c52c3e8abb2
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-02 10:51:09 -0300

    primwri conponete

---
+++ src/UI/Shop/MarcacaoDePagina.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../../CSS/Shop/marcacaoDePAgina.css"
+
+const MarcacaoDePagina = () => {
+  return (
+    <div><h3 className='tituloShop'>Shop / Products</h3></div>
+  )
+}
+
+export default MarcacaoDePagina
