d9fdaa3c2e84ee961c541ee6e8536fa6ee2f6b69
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-02 11:32:41 -0300

    divs

--- src/UI/Shop/ListaDeProdutos.tsx
+++ src/UI/Shop/ListaDeProdutos.tsx
@@ -24,30 +24,42 @@ const ListaDeProdutos = (props: Props) => {
 
 
             <div className="flexbox">
-            <img src={img1} alt="" />
 
-            <img src={img2} alt="" />
+    <div className="divsConteudo">
+    <img src={img1} alt="" />
 
-            <img src={img3} alt="" />
+<img src={img2} alt="" />
 
-            <img src={img4} alt="" />
+<img src={img3} alt="" />
 
-            <img src={img5} alt="" />
+<img src={img4} alt="" />
+    </div>
 
-            <img src={img6} alt="" />
+    <div className="divsConteudo">
+    <img src={img5} alt="" />
 
-            <img src={img7} alt="" />
+<img src={img6} alt="" />
 
-            <img src={img8} alt="" />
+<img src={img7} alt="" />
 
-            <img src={img9} alt="" />
+<img src={img8} alt="" />
+        </div>
 
-            <img src={img10} alt="" />
+        <div className="divsConteudo">
+        <img src={img9} alt="" />
 
-            <img src={img11} alt="" />
+<img src={img10} alt="" />
 
-            <img src={img12} alt="" />
-            
+<img src={img11} alt="" />
+
+<img src={img12} alt="" />
+        </div>
+
+
+
+
+
+
             </div>
 
     </div>
