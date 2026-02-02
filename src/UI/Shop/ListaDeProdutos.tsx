7245c4c3ebf5f8ee7ce6540a13162b2cba9fb691
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-02 11:23:53 -0300

    rota loja

---
+++ src/UI/Shop/ListaDeProdutos.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import img1 from "../../assets/imgShop/perfumeLoja1.png"
+type Props = {}
+
+const ListaDeProdutos = (props: Props) => {
+  return (
+    <div>
+
+<img src={img1} alt="" />
+
+    </div>
+  )
+}
+
+export default ListaDeProdutos
