d711d3b661542564e1899802bacc1595dba589d1
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-02 10:20:03 -0300

    nova rota

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,6 @@ import "../CSS/Home/mainPerfumesMaisRecentes.css"
 
 
 
-import imgNossosValores from "../assets/imgs/imgNossosValores.png"
 import imgProdutos from "../assets/imgs/Frame 280.png"
 import img1 from "../assets/imgs/Group 14.png"
 import img2 from "../assets/imgs/wcwsc 1.png"
@@ -26,6 +25,8 @@ import img9 from "../assets/imgs/imgPerfumesRecentes.png"
 
 import ConteudoInicial from "../UI/Home/ConteudoInicial"
 import BoasVindas from "../UI/Home/BoasVindas"
+import NossosValores from "../UI/Home/NossosValores"
+
 import Footer from "../components/Footer"
 
 const Home = () => {
@@ -36,30 +37,7 @@ const Home = () => {
 
           <BoasVindas />
 
-          <main className="nossosValores">
-            <img src={imgNossosValores} className='imgNossosValores' alt="" />
-              <div className="conteudoNossosValores">
-          <h2 className='titutloNossoValores'>Our Values</h2>
-
-
-      <div className="conteudoDosParagrafos">
-            <p className="nossoValoresParagrafo">
-                At Local Face, our perfume retail store is built on a foundation of passion and authenticity.
-                 We believe in celebrating the individuality of every customer, providing a diverse collection
-                 of scents that resonate with their unique personality and style. Our dedicated team of fragrance
-                  enthusiasts is committed to creating a welcoming and inclusive environment, where connections are forged, and inspiration thrives.
-                </p>
-
-
-                <p className="nossoValoresParagrafo">
-                Embracing sustainability and continuous learning, Local Face strives to be more than just a shopping destination; we are a community that inspires and empowers individuals on their fragrance journey.
-                  </p>
-
-      </div>
-
-              </div>
-
-          </main>
+          <NossosValores />
 
 
           <main className="mainAmostraProdutos">
