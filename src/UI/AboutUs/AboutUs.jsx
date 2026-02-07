5bb0a1b357f0998e41b0fe2484d0f9fe0956856f
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-07 08:57:49 -0300

    improtções about us

---
+++ src/UI/AboutUs/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../../CSS/About/aboutUs.css"
+
+type Props = {}
+
+const AboutUs = (props: Props) => {
+  return (
+    <div>AboutUs</div>
+  )
+}
+
+export default AboutUs
