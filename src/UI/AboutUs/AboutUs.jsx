58fe4db5ef76ae520f982bdd57b4d768aea4ea9b
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-07 09:17:10 -0300

    colcoando em divs

--- src/UI/AboutUs/AboutUs.jsx
+++ src/UI/AboutUs/AboutUs.jsx
@@ -6,9 +6,14 @@ type Props = {}
 const AboutUs = (props: Props) => {
   return (
     <div className='aboutUsSection'>
-        <h1>About US</h1>
+                <div className="titulo">
+                <h1>About US</h1>
+                </div>
 
-        <p>At Local Face, we believe that perfumes are more than just scents; they are expressions of one's individuality and style. Our passion for exquisite fragrances led us to curate a collection that captures the essence of diverse personalities, bringing you an unparalleled olfactory experience.</p>
+       <div className="paragrafo">
+       <p>At Local Face, we believe that perfumes are more than just scents; they are expressions of one's individuality and style.
+        Our passion for exquisite fragrances led us to curate a collection that captures the essence of diverse personalities, bringing you an unparalleled olfactory experience.</p>
+       </div>
     </div>
   )
 }
