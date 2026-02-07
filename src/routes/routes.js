37c52f26318848dabca036745df2acdbe51f039b
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-07 08:40:22 -0300

    nova page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Header from '../components/Header';
 import Home from '../pages/Home';
 import Shop from '../pages/Shop';
+import AboutUs from '../pages/AboutUs';
 
 const AppRoutes = () => {
   // Isso verifica se você está no computador (localhost) ou no GitHub
@@ -14,6 +15,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/aboutus" element={<AboutUs />} />
 
         {/* Rota de segurança: se digitar algo errado, volta para a Home */}
         <Route path="*" element={<Navigate to="/" replace />} />
