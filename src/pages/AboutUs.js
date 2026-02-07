a29e8ed6a13c7af1d101177ffc7113c256b03f28
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-07 09:24:06 -0300

    novo conteudo do aboute us

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import MarcacaoDepagina from "../../src/UI/AboutUs/MarcacaoDePagina"
 import Aboutus from "../../src/UI/AboutUs/AboutUs.jsx"
+import OurStory from "../../src/UI/AboutUs/OurStory.jsx"
 
 const AboutUs = () => {
   return (
