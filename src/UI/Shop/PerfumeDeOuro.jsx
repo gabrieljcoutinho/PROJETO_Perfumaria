2154b8bb0f7e672e9dd9192dc04b0be4f5ef8142
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-07 07:41:25 -0300

    conteudo

--- src/UI/Shop/PerfumeDeOuro.jsx
+++ src/UI/Shop/PerfumeDeOuro.jsx
@@ -14,7 +14,8 @@ const PerfumeDeOuro = (props: Props) => {
           <img src={imgDeOuro} alt=""  className='imgPerfumeDeOuro'/>
               <div className="textoConteudoPerfumeDeouro">
                   <h1>Limited Time Offer: 25% OFF on Golden Algel Perfume</h1>
-
+                  <h2>Golden Algel</h2>
+                  <p className="paragrafoDeOuro">Unleash Your Divine Glow</p>
               </div>
           </main>
 
