6dc0f101d181c358740a451394cbf87032537e6f
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-07 09:53:18 -0300

    br

--- src/UI/AboutUs/OurStory.jsx
+++ src/UI/AboutUs/OurStory.jsx
@@ -7,7 +7,11 @@ const OurStory = (props: Props) => {
   return (
     <div className='ourStoryDiv'>
         <h2>Our Story</h2>
-        <p className='paragrafoOurStory'>Local Face Perfumes was founded by a group of perfume enthusiasts with a shared vision to create a haven for perfume lovers seeking authentic, locally-inspired fragrances. Inspired by the diversity and richness of cultures around the world, we set out on a journey to curate a collection of scents that capture the essence of each region. Our aim is to bring you closer to the heart and soul of different cultures through the art of perfumery.</p>
+        <p className='paragrafoOurStory'>Local Face Perfumes was founded by a group of perfume enthusiasts with a shared vision to <br />
+            create a haven for perfume lovers seeking authentic, locally-inspired fragrances. Inspired by<br />
+            the diversity and richness of cultures around the world, we set out on a journey to curate a<br />
+            collection of scents that capture the essence of each region. Our aim is to bring you closer<br />
+            to the heart and soul of different cultures through the art of perfumery.</p>
     </div>
   )
 }
