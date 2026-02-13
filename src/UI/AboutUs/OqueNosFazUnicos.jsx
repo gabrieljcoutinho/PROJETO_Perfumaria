3c85188f6e779b19b212ac58a0f2112d005f7fb5
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-13 11:20:37 -0300

    lista

--- src/UI/AboutUs/OqueNosFazUnicos.jsx
+++ src/UI/AboutUs/OqueNosFazUnicos.jsx
@@ -7,6 +7,16 @@ const OqueNosFazUnicos = (props: Props) => {
   return (
     <div className='diferencial'>
         <h3 className="tituloOqueNosFazDiferentes">What Makes Us Unique</h3>
+
+            <ul className="conteudoUnicos">
+                    <li></li>
+
+                    <li></li>
+
+                    <li></li>
+
+            </ul>
+
         </div>
   )
 }
