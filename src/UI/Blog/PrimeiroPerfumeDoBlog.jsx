58ff73d5ea770220a971fafc13a141d299b84fc4
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-18 11:50:36 -0300

    classe

--- src/UI/Blog/PrimeiroPerfumeDoBlog.jsx
+++ src/UI/Blog/PrimeiroPerfumeDoBlog.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const PrimeiroPerfumeDoBlog = () => {
   return (
-    <div>PrimeiroPerfumeDoBlog</div>
+    <div className='primeriPerfumeBlog'>PrimeiroPerfumeDoBlog</div>
   )
 }
 
