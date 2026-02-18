287b5a1d58e9bf2d23d91f1d5b93a303bfb81435
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-18 11:20:57 -0300

    renomena aruqivorqiov

--- src/UI/Blog/ConteudoTodoPrincipal.jsx
+++ src/UI/Blog/ConteudoTodoPrincipal.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Titulo from '../UI/Blog/Titulo.jsx'
-import TituloPrincipal from '../UI/Blog/TituTituloPrincipallo.jsx'
+import TituloPrincipal from '../UI/Blog/TituloPrincipal.jsx'
 
 const ConteudoTodoPrincipal = () => {
   return (
