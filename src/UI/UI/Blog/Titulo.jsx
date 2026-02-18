c29d3b02a5c54d1cc5592833be272de8d9b2a374
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-18 11:14:44 -0300

    css

--- src/UI/UI/Blog/Titulo.jsx
+++ src/UI/UI/Blog/Titulo.jsx
@@ -4,7 +4,7 @@ import  "../../../CSS/Blog/titulo.css"
 const Titulo = () => {
   return (
     <div>
-      <h3 className='tituloBlog'>wedjhalksjdhkjl</h3>
+      <h3 className='tituloBlog'>January 5, 2023 <br/> Perfume Collections</h3>
     </div>
   )
 }
