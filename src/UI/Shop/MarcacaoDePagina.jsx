276f804c9ef7863d2e2e3c93116491bbd838b834
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-18 11:02:20 -0300

    renomeando

---
+++ src/UI/Shop/MarcacaoDePagina.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../../CSS/Shop/marcacaoDePAgina.css";
+
+const MarcacaoDePagina = () => {
+  return (
+    <div>
+
+      <h3 className='tituloShop'>Home / Products</h3>
+    </div>
+  );
+};
+
+export default MarcacaoDePagina;
