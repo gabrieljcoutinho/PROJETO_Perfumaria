b2dad915e16032d315c4c8722bad31d3c65f51b7
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-18 11:02:30 -0300

    renomeando

---
+++ src/UI/Shop/TituloPrincipalDaPaginaShop.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../../CSS/Shop/tituloPrincipal.css"
+
+
+const TituloPrincipalDaPaginaShop = () => {
+  return (
+    <div>
+        <h2>Best Selling Products</h2>
+    </div>
+  )
+}
+
+export default TituloPrincipalDaPaginaShop
