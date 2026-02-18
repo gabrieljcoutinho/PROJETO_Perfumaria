d05d59ee12deb4ab2032d57fadacdbcb38315079
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-18 10:35:12 -0300

    h1

---
+++ src/UI/Blog/marcacaoDePagina.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+const marcacaoDePagina = (props: Props) => {
+  return (
+    <div>
+
+        <h1>Home / Blog / The Art of Curating a Luxury Perfume Collections A Symphony os Scenes and Stories</h1>
+
+    </div>
+  )
+}
+
+export default marcacaoDePagina
