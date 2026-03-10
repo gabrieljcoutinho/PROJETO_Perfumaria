443ae492e527d0937c5be79bde6c912c53ead779
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-10 09:36:56 -0300

    refaxnedo rotas

---
+++ src/UI/Blog/Titulo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import  "../../../CSS/Blog/titulo.css"
+
+const Titulo = () => {
+  return (
+    <div>
+      <h3 className='tituloBlog'>January 5, 2023 <br/> Perfume Collections</h3>
+    </div>
+  )
+}
+
+export default Titulo
