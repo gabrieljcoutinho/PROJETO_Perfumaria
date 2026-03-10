fb45b067c6db733078212c96a500efb0583d2d74
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-10 09:27:46 -0300

    teste

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import MarcacaoDepagina from "../UI/AboutUs/marcacaoDePagina"
-import Aboutus from "../UI/AboutUs/AboutUs.jsx"
+import Aboutus from "../UI/AboutUs/AboutUs.js"
 import OurStory from "../UI/AboutUs/OurStory.jsx"
 import AmostraDeBebida from "../UI/AboutUs/AmostraDeBebida.jsx"
 import OqueNosFazUnicos from "../UI/AboutUs/OqueNosFazUnicos.jsx"
