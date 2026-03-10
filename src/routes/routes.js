6e9e3167f90a23b83fe7e2df92039eefd5a91b33
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-10 09:45:37 -0300

    detalhe

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
 import Header from '../components/Header';
 import Home from '../pages/Home';
 import Shop from '../pages/Shop';
 import AboutUs from '../pages/AboutUs';
 import Blog from '../pages/Blog';
 
-import ScrollToTop from "../../src/utils/ScrollToTop.js"
+import ScrollToTop from '../utils/ScrollToTop';
 
 const AppRoutes = () => {
-  // Isso verifica se você está no computador (localhost) ou no GitHub
-  const base = window.location.hostname === 'localhost' ? '/' : '/Perfumaria';
-
   return (
-    <BrowserRouter basename={base}>
-      <Header />
+    <BrowserRouter>
       <ScrollToTop />
+      <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/blog" element={<Blog />} />
 
-        {/* Rota de segurança: se digitar algo errado, volta para a Home */}
+        {/* Se digitar rota errada volta para home */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
