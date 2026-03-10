1c786f5f84442cdb4a3790eba6c40b8a60f12b07
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-10 09:36:58 -0300

    refaxnedo rotas

---
+++ src/UI/Blog/TituloPrincipal.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../../../CSS/Blog/tituloPrincipal.css"
+
+
+
+const TituloPrincipal= () => {
+  return (
+    <div>
+        <h3>The Art of Curating a luxuary Perfume Collection: A Symphony of Scents snd Storiestitulo</h3>
+    </div>
+  )
+}
+
+export default TituloPrincipal
