b2e41fb09a6c5e3e1ba19f6b422b1d21ce85bc0a
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-10 09:27:51 -0300

    teste

---
+++ src/pages/Shop.js
@@ -0,0 +1,26 @@
+import React from 'react';
+// Removida a extensão .tsx da importação
+import MarcacaoDePagina from '../UI/Shop/MarcacaoDePagina.jsx';
+import TituloPrincipalDaPaginaShop from "../UI/Shop/TituloPrincipalDaPaginaShop.jsx"
+import ListaProdutos from "../UI/Shop/ListaDeProdutos.jsx"
+import OfertaEspecial from "../UI/Shop/OfertaEspecial.jsx"
+import PerfumeDeOuro from "../UI/Shop/PerfumeDeOuro.jsx"
+import Footer from "../components/Footer.js"
+
+
+
+const Shop = () => {
+  return (
+    <div>
+      <MarcacaoDePagina />
+      <TituloPrincipalDaPaginaShop />
+      <ListaProdutos />
+      <OfertaEspecial />
+      <PerfumeDeOuro />
+
+<Footer />
+    </div>
+  );
+};
+
+export default Shop;
